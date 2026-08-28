1f1381115b9dee52bc049e98c9260dc271c48f38
xdde123 <63497896+xdde123@users.noreply.github.com>
2026-08-28 21:36:59 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,4 +18,5 @@ android {
 dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.tom-roush:pdfbox-android:2.0.27.0")
  }

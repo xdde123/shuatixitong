0e4e1416cb0bc37ff02b1b575147ce1161e3cfa0
xdde123 <63497896+xdde123@users.noreply.github.com>
2026-08-28 20:48:24 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,22 @@
-plugins { id("com.android.application") }
+plugins {
+    id("com.android.application")
+}
 
-android { namespace = "com.guanlixitong.app"; compileSdk = 35
- defaultConfig { applicationId = "com.guanlixitong.app"; minSdk = 24; targetSdk = 35; versionCode = 2; versionName = "2.0" }
+android {
+    namespace = "com.guanlixitong.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.guanlixitong.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
 }
 
 dependencies {
- implementation("androidx.core:core-splashscreen:1.0.1")
- implementation("androidx.core:core:1.13.1")
- implementation("com.tom-roush:pdfbox-android:2.0.27.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.github.bumtech:android-view:0.27.0")
 }
